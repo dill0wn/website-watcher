edb62e7b24960f046299db6d31fa6880d5214f2f
Dillon Walls <dillon.walls@slashdotmedia.com>
2020-10-02 09:53:30 -0400

    WIP on selenium strategy

--- watcher.py
+++ watcher.py
@@ -1,9 +1,35 @@
+import os
+from selenium import webdriver
+import logging
 from urllib import request
-from bs4 import BeautifulSoup  # Requires beautifulsoup4
 
-page = request.urlopen('https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3080/').read()
-soup = BeautifulSoup(page, features='html.parser')
 
-# will always be 'ADD TO CART because JS updates. 
-# need selenium or other web driver
-print(soup.select('.btn-cart')[0].get_text())
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime) %(levelname)-5.5s: %(message)s",
+    handlers=[logging.StreamHandler()]
+)
+logger = logging.getLogger(__name__)
+
+url = 'https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3080/'
+
+
+def is_element_present(driver, selector, wait_time=None):
+    driver.find_elements_by_css_selector('selector')
+
+
+def main():
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver_path = '/usr/lib/chromium-browser/chromedriver'
+    if not os.path.isfile(driver_path):
+        raise FileNotFoundError('Chromedriver not installed')
+
+    driver = webdriver.Chrome(
+        executable_path=driver_path, chrome_options=options)
+    driver.get(url)
+    is_element_present(driver, '.btn.show-out-of-stock', )
+
+
+if __name__ == '__main__':
+    main()
